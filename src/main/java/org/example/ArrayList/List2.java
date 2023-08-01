f82d77e948f60d6d140f34ae4ad66a14c38b3f2a
harikrishnanmuruga <121540050+harikrishnanmuruga@users.noreply.github.com>
2023-08-01 09:51:31 +0530

    add some line

--- src/main/java/org/example/ArrayList/List2.java
+++ src/main/java/org/example/ArrayList/List2.java
@@ -7,13 +7,19 @@ public class List2 {
         ArrayList L1=new ArrayList();
         L1.add(1);
         L1.add(2);
+        L1.add(5);
+        L1.add(6);
         System.out.println(L1);
         ArrayList L2=new ArrayList();
         L2.add(11);
         L2.add(12);
+        L2.add(14);
+        L2.add(15);
         System.out.println(L2);
         L1.addAll(L2);
         System.out.println(L1);
+
+        System.out.println("contains 5 in L1 : " +L1.contains(5));
         
     }
 }
